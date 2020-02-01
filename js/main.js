161cf139577285db7fe3c7e6aba357f0ef5f8a89
Johan Bos <bosbosjohan@gmail.com>
2020-02-01 22:36:43 +0100

    Version 2.0

--- js/main.js
+++ js/main.js
@@ -1,65 +1,39 @@
-$(document).ready(function () {
-    $(document).on("scroll", function () {
-        if ($(document).scrollTop() > 86) {
-            $(".navbar").addClass("shrink");
-        }
-        else {
-            $(".navbar").removeClass("shrink");
-        }
-    });
+let slideIndex = 1;
 
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 30,
-        dots: true,
-        nav: false,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                margin: 10,
-                stagePadding: 20,
-            },
-            768: {
-                items: 3,
-                margin: 20,
-                stagePadding: 50,
-            },
-            1000: {
-                items: 4
-            }
-        }
-    });
+document.addEventListener(
+  "DOMContentLoaded",
+  function() {
+    showSlides(slideIndex);
+  },
+  false
+);
 
-    $("#projects .item-image").on("click", function () {
-        var modal = $("#imgModal");
-        var img = $(this).attr("src");
-        console.log(img);
-        var modalImg = $("#modalImg");
-        var captionText = $("#caption");
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-        modal.css("display", "block");
-        modalImg.attr("src", img);
-        captionText.html(this.alt);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
-        var span = $("#imgModal > span");
-        span.on("click", function () {
-            modal.css("display", "none");
-        });
-    });
-
-    let anchorlinks = $(".navbar a.nav-link");
-    for (let item of anchorlinks) {
-        $(item).on("click", function (e) {
-            e.preventDefault();
-            let hashval = $(this).attr("href");
-            let target = $(hashval);
-            target = target.length ? target : $('[name=' + hashval.slice(1) + ']');
-            $("html, body").animate({ scrollTop: $(target).offset().top - 100 }, 'slow');
-        });
-    }
-});
-
-
-
-
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
